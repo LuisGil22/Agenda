cc58622d3e9974df1a61c7f3293a28587a03baa4
Marta <marta@gmail.com>
2025-05-10 16:44:39 +0200

    Control d’errors per camps buits al formulari

--- script.js
+++ script.js
@@ -2,13 +2,22 @@
 
 console.log("Agenda Web");
 
-const contactes = ["Jose", "Saida", "Ruben"];
-const llista = document.createElement('ul');
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  const input = document.querySelector("#nom");
+  if (input.value.trim() === "") {
+    alert("El camp no pot estar buit");
+    return;
+  }
+  // Lògica per afegir el contacte...
+  const contactes = ["Jose", "Saida", "Ruben"];
+  const llista = document.createElement('ul');
 
-contactes.forEach(nom => {
-  const li = document.createElement('li');
-  li.textContent = nom;
-  llista.appendChild(li);
+  contactes.forEach(nom => {
+    const li = document.createElement('li');
+    li.textContent = nom;
+    llista.appendChild(li);
+  });
+
+  document.body.appendChild(llista);
 });
-
-document.body.appendChild(llista);
